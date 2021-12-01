3a7b79d7ac97d7dae2cddaddf9bde89c4051197c
abelfd <abel.flores@institutvidreres.cat>
2021-12-01 13:25:56 +0100

    continuacion

---
+++ Ex4.js
@@ -0,0 +1,12 @@
+var x = prompt("Pon un numero");
+var suma = 0;
+var n = 0;
+while (suma<x){
+    console.log(n)
+    n++;
+    suma=suma+n;
+}
+console.log(n)
+console.log("n es " + n )
+console.log("x es " + x )
+
